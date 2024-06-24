9ee595dfd3bed975f6ace5399a6939b2a374c237
yasso <ahmedoyassin02@gmail.com>
2024-06-24 05:43:46 +0300

    adding basic file structure print hello world in cpp language

---
+++ 01_CPP_BASICS/01-c++-file-structure/print_hello_world.cpp
@@ -0,0 +1,27 @@
+
+
+
+/**
+ * Print Hello World Screen
+ */
+
+
+// a. pre-processor directives
+// a.1. dependency.
+#include <iostream>
+
+// a2. Macros
+#define NAME "YASSIN"
+
+// a3. Conditional compilations (text removal)
+// #ifdef
+
+// 2. decleration statements
+
+// 3. main entry point
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+}
